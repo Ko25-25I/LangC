fb963ec025b0d53cdc54a3c53ec0bc16d337616d
Ko25-25I <128129610+Ko25-25I@users.noreply.github.com>
2023-11-24 13:55:12 +0900

    Add files via upload

---
+++ comprehensive_test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void){
+    int num;
+
+    printf("1~50までの数字で3で割り切れる数字は以下です。\n");
+    for(num = 1; num <= 25; num++){
+        if(num % 3 == 0){
+            printf("%d \n", num);
+        }
+    }
+    while(num <= 50){
+        if(num % 3 == 0){
+            printf("%dは3で割り切れます。", num);
+        }
+    }
+
+
+    return 0;
+}
